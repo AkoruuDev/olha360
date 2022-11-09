f5c37ad757dae73364ab2530f96968aeb861b618
Akoruu.Dev <akoruu.dev@gmail.com>
2022-11-08 23:27:12 -0300

    STYLE [Header]: Stylize Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,74 @@
+import styled from "styled-components"
+
 export default function Header() {
     return (
-        <>
-            <>
+        <Container>
+            <Content>
                 <img src={'Logo aqui'} alt='olhar360-logo' />
-                <nav>
-                    <p>Home</p>
-                    <p>Sobre</p>
-                    <p>Contato</p>
-                </nav>
-            </>
-        </>
+                <Nav>
+                    <div><p>Home</p></div>
+                    <div><p>About</p></div>
+                    <div><p>Contact</p></div>
+                </Nav>
+            </Content>
+            <Div></Div>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    width: 100vw;
+    background-color: #4784C1;
+
+    font-size: 16px;
+    font-weight: 700;
+    color: #D8EAFB;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+const Content = styled.div`
+    width: 80vw;
+    padding: 15px 0;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    position: fixed;
+    z-index: 1;
+    top: 0;
+`
+
+const Div = styled.div`
+    width: 65vw;
+    border: 1px solid #D8EAFB;
+`
+
+const Nav = styled.nav`
+    width: 50%;
+    height: 25px;
+    margin: 15px 0;
+
+    display: flex;
+    justify-content: space-between;
+
+    & div {
+        width: 30%;
+        display: flex;
+        justify-content: center;
+    }
+
+    & p {
+        transition: all 0.05s linear;
+    }
+
+    & p:hover {
+        cursor: pointer;
+        font-size: 18px;
+        color: #EA9EE4;
+    }
+`
