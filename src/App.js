e36d0f6cee92d2002a097f2351b979446b03706e
Akoruu.Dev <akoruu.dev@gmail.com>
2022-11-08 22:27:13 -0300

    FEAT [Connection]: Update App.js file and connect components on App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+// tools
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GlobalStyle from "./styles/globalStyle";
+
+// components
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+// pages
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Contacts from "./components/pages/Contact";
+
 export default function App() {
     return (
-        <></>
+        <>
+            <GlobalStyle />
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Main>
+                        <Route path={'/'} element={<Home />} />
+                        <Route path={'/about'} element={<About />} />
+                        <Route path={'/contact'} element={<Contacts />} />
+                    </Main>
+                </Routes>
+                <Footer />
+            </BrowserRouter>
+        </>
     )
 }
